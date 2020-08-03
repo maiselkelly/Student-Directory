33092d4cbcfe987618897a5ac1498e6d5551fcd9
maiselkelly <maiselkelly@gmail.com>
2020-08-03 14:25:01 -0400

    Initial commit

---
+++ src/StudentDirectory/StudentDao.java
@@ -0,0 +1,18 @@
+package StudentDirectory;
+
+import java.util.List;
+
+public interface StudentDao {
+
+    public List<Student> readAllStudents();
+    public void showAllStudents();
+    public void addNewStudent(Student student);
+    public void updateStudent(String updateStudent);
+    public void deleteStudent(int id);
+    public Student findStudentID(int id);
+    public List <Student> findStudentFirstName(String firstName) throws Exception;
+    public List <Student> findStudentLastName(String lastName) throws Exception;
+    public void writeToFile();
+}
+
+
